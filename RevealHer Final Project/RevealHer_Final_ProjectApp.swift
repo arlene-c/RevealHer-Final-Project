d6757486074fdab6746d0155a148d4d525e71f93
arlene <arlene2k7@gmail.com>
2023-06-27 14:14:37 -0700

    Initial Commit

---
+++ RevealHer Final Project/RevealHer_Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevealHer_Final_ProjectApp.swift
+//  RevealHer Final Project
+//
+//  Created by Arlene Chang on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct RevealHer_Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
