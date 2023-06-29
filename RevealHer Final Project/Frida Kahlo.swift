cdff1fd247f33fec5051306bcb933db6ebffd6e1
Katherine Hwang <kateahwang@gmail.com>
2023-06-29 10:30:06 -0700

    Frida Kahlo

--- RevealHer Final Project/Frida Kahlo.swift
+++ RevealHer Final Project/Frida Kahlo.swift
@@ -1,5 +1,5 @@
 //
-//  Dororthy Arzner.swift
+//  Frida Kahlo.swift
 //  RevealHer Final Project
 //
 //  Created by Kate Hwang on 6/29/23.
@@ -7,31 +7,31 @@
 
 import SwiftUI
 
-struct Dororthy_Arzner: View {
+struct Frida_Kahlo: View {
     var body: some View {
         ZStack {
             Color("mid-purple")
                 .ignoresSafeArea()
             VStack(alignment: .center, spacing: 40.0) {
-                Image("DorothyArzner")
+                Image("Frida Kahlo")
                     .resizable()
                     .aspectRatio(contentMode:.fit)
                     .cornerRadius(18)
                 HStack {
-                    Text("Dorothy Arzner")
+                    Text("Frida Kahlo")
                         .font(.title)
                         .fontWeight(.heavy)
                         .multilineTextAlignment(.center)
-                        
-                    Text("The 1930s Queen")
+                    
+                    Text("The Vibrant Painter")
                         .multilineTextAlignment(.center)
                 }
                 
-                Text("Dorothy Arzner was the first woman to join the Directors Guild of American and the first woman to direct a sound film. She made a total of twenty films between 1927 and 1943, launching the careers of a number of Hollywood actresses, including Katharine Hpeburn, Rosalind Russell, and Lucille Ball.")
+                Text("Frida Kahlo was a Mexican artist, and is remembered for her self-portraits and use of bold, vibrant colors. She loved to depict the female experience in her work. She was one of the first female artists to rebel against the current art status quo in order to explore her passion.")
                     .fontWeight(.light)
                     .multilineTextAlignment(.center)
-            
-                   
+                
+                
                 
             }
             .padding()
@@ -40,10 +40,10 @@ struct Dororthy_Arzner: View {
             .padding()
         }
     }
-}
-
-struct Dororthy_Arzner_Previews: PreviewProvider {
-    static var previews: some View {
-        Dororthy_Arzner()
+    
+    struct Frida_Kahlo_Previews: PreviewProvider {
+        static var previews: some View {
+            Frida_Kahlo()
+        }
     }
 }
