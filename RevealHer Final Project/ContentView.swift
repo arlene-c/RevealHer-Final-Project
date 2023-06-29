f06a1e30df98a1164198fa5a78ec4218089e64b1
arlene <arlene2k7@gmail.com>
2023-06-29 14:38:23 -0700

    background color

--- RevealHer Final Project/ContentView.swift
+++ RevealHer Final Project/ContentView.swift
@@ -8,76 +8,112 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     var body: some View {
-        NavigationStack {
-            VStack(alignment: .center) {
-                Text("RevealHer")
-                    .font(.title)
-                    .fontWeight(.bold)
-                
-                Image("women")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                
-                
-                Spacer()
-                Text("Welcome to RevealHer")
-                    .font(.title)
-                    .fontWeight(.regular)
-                    .fontDesign(.rounded)
-                Text("Select a Field")
-                    .multilineTextAlignment(.leading)
-                
-                
-                ZStack {
-                    HStack() {
-                        Spacer()
-                        NavigationLink(destination: STEMHomepage()) {
-                            Image("beaker")
+        ZStack {
+            Color("light")
+                .ignoresSafeArea()
+            NavigationStack {
+                        VStack(alignment: .center) {
+                            
+                            Text("RevealHer")
+                                .font(.custom("AbrilFatface-Regular", size:60))
+                                .fontWeight(.bold)
+                                
+                            
+                            Image("women")
                                 .resizable()
                                 .aspectRatio(contentMode: .fit)
-                                .frame(width:60)
-                                .padding()
-                                .background(Rectangle()
-                                    .foregroundColor(.gray))
-                                .cornerRadius(15)
+                            
+                                
+                            Spacer()
+                            Text("Welcome to RevealHer!")
+                                .font(.custom("AbrilFatface-Regular", size:30))
+                            Text("Select a Field to Learn More")
+                                .multilineTextAlignment(.leading)
+                                .font(.title3)
+                            Spacer()
+                            Spacer()
+                            
+                            ZStack {
+                                HStack() {
+                                    Spacer()
+                                    NavigationLink(destination: STEMHomepage()) {
+                                        Image("beaker")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width:60)
+                                            .padding()
+                                            .background(Rectangle()
+                                                .foregroundColor(Color("chinese violet"))
+                                                .cornerRadius(15))
+                                            .shadow(radius:15)
+                                    }
+                                    Spacer()
+                                    NavigationLink(destination:ArtsHomepage()) {
+                                        Image("brush clipart")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width:50)
+                                            .padding()
+                                            .background(Rectangle()
+                                                .foregroundColor(Color("chinese violet")))
+                                            .cornerRadius(15)
+                                            .shadow(radius:15)
+                                    }
+                                    Spacer()
+                                    NavigationLink(destination:LeadershipHomepage()){
+                                        Image("briefcase")
+                                            .resizable()
+                                            .aspectRatio(contentMode:.fit)
+                                            .frame(width:70)
+                                            .padding()
+                                            .background(Rectangle()
+                                                .foregroundColor(Color("chinese violet")))
+                                            .cornerRadius(15)
+                                            .shadow(radius:15)
+                                    }
+                                    Spacer()
+                                }
+                            }
+                            
+                            HStack() {
+                               Spacer()
+                               Spacer()
+                                Text("STEM")
+                                    .font(.custom("Montserrat-Italic-VariableFont_wght", size:20))
+                                    
+                                Spacer()
+                                Spacer()
+                                Text("Arts & Media")
+                                    .font(.custom("Montserrat-Italic-VariableFont_wght", size:20))
+                                Spacer()
+                                Text("Leadership")
+                                    .font(.custom("Montserrat-Italic-VariableFont_wght", size:20))
+                                Spacer()
+                            }
+                            
+                            
+                            Spacer()
                         }
-                        Spacer()
-                        NavigationLink(destination:ArtsHomepage()) {
-                            Image("brush clipart")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width:50)
-                                .padding()
-                                .background(Rectangle()
-                                    .foregroundColor(.gray))
-                                .cornerRadius(15)
-                        }
-                        Spacer()
-                        NavigationLink(destination:LeadershipHomepage()){
-                            Image("briefcase")
-                                .resizable()
-                                .aspectRatio(contentMode:.fit)
-                                .frame(width:70)
-                                .padding()
-                                .background(Rectangle()
-                                    .foregroundColor(.gray))
-                                .cornerRadius(15)
-                        }
-                        Spacer()
                         
-                    }
+ 
                 }
-                
-                
-                Spacer()
-                Spacer()
-                Spacer()
-                
-            }
-            
-            
+                .background(Rectangle() . foregroundColor(.white))
+                       .cornerRadius(18)
+                       .padding()
         }
+        
+
+        
+        
+      
+               
+                   
+
+        
+
     }
     
     struct ContentView_Previews: PreviewProvider {
